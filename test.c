6b1df9de0f1d9c94672451475fd3f91cd7b54cad
jgervais4 <jgervais@bloomberg.net>
2014-02-08 14:49:45 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 //banana
-
+//HBK smells funny
 int main()
 {
     
     int a = 1;
     a = 8;// co
-    
-    
     mment kjhkjh
     printf("Some number: %d", a);
     return 0;
